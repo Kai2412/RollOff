305032c5463073acdb5d508f80f5d435bbd98c5e
Dustin Dutton <61857251+Kai2412@users.noreply.github.com>
2025-01-09 00:19:37 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+var randomNumber1 = Math.floor((Math.random()*6)) + 1; // 1-6 Random
+var randomNumber2 = Math.floor((Math.random()*6)) + 1;
+
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";// dicevar.png combies dice with the var and adds .png to give an img
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
+
+var randomImageSource1 = "images/" + randomDiceImage1; // adds images/ to the previous variable could be done in the step before
+var randomImageSource2 = "images/" + randomDiceImage2;
+
+document.querySelectorAll("img")[0].src = randomImageSource1;
+document.querySelectorAll("img")[1].setAttribute("src",randomImageSource2);
+if(randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins!!";
+}
+    else if(randomNumber1 < randomNumber2){
+    document.querySelector("h1").innerHTML = " Player 2 wins!!🚩";
+}
+    else{
+        document.querySelector("h1").innerHTML = "🧨Draw!🧨";
+    }
